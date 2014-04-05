b0bd6088e75d002d8db09d68937c085b27065ffd
Dariel Javier <dariel.javier@theberrycompany.com>
2014-04-05 10:19:49 -0400

    Initial Commit

---
+++ chinchini/chinchini/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chinchini.MvcApplication" Language="C#" %>
---
+++ chinchini/chinchini/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chinchini.Startup))]
+namespace chinchini
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
