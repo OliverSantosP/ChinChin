035148bd68db7ad765950d823187fde463d4ae3f
Dariel Javier <dariel.javier@theberrycompany.com>
2014-04-06 07:08:49 -0400

    Add Group CRUD

---
+++ chinchini/chinchini/Controllers/GroupController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using chinchini.Models;
+
+namespace chinchini.Controllers
+{
+    public class GroupController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Group/
+        public ActionResult Index()
+        {
+            var group = db.UserGroup.Where(x => x.User.UserName == User.Identity.Name).FirstOrDefault();
+
+            ViewBag.GroupID = group == null ? 0 : group.GroupID;
+            return View(db.Group.Include("UserGroups").ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Join(int id)
+        {
+            var user = db.Users.FirstOrDefault(x=>x.UserName == User.Identity.Name);
+
+            db.UserGroup.Add(new UserGroup { GroupID = id, User_Id = user.Id });
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult UnJoin(int id)
+        {
+             var user = db.Users.FirstOrDefault(x=>x.UserName == User.Identity.Name);
+             var userGroup = db.UserGroup.Where(x => x.User_Id == user.Id && x.GroupID == id).FirstOrDefault();
+
+             db.UserGroup.Remove(userGroup);
+             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Group/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Group.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // GET: /Group/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Group/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="GroupID,Name")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Group.Add(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(group);
+        }
+
+        // GET: /Group/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Group.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: /Group/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="GroupID,Name")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(group);
+        }
+
+        // GET: /Group/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Group.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: /Group/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Group group = db.Group.Find(id);
+            db.Group.Remove(group);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- chinchini/chinchini/Models/UserGroup.cs
+++ chinchini/chinchini/Models/UserGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,12 @@ namespace chinchini.Models
     public class UserGroup
     {
         public int UserGroupID { get; set; }
+        public int GroupID { get; set; }
+
+        [ForeignKey("User")]
+        public string User_Id { get; set; }
+
+        public virtual Group Group { get; set; }
         public virtual ApplicationUser User { get; set; }
         
     }
